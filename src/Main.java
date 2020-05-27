c02d457c4f7186ac53097b8b1c0715669b020183
giannakop <65922345+giannakop@users.noreply.github.com>
2020-05-27 21:43:24 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import java.util.*;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 7_USER
+ */
+public class Main {
+    
+    
+    public static ArrayList<User> userlist = new ArrayList<User>();
+    public static ArrayList<Administrator> adminlist = new ArrayList<Administrator>();
+    public static ArrayList<DisplayMessageListForm> usermessagelist= new ArrayList<DisplayMessageListForm>();
+    public static ArrayList<Book> booklist = new ArrayList<Book>();
+    public static ArrayList<Booking> bookinglist = new ArrayList<Booking>();
+    public static ArrayList<Reservation> reservationlist = new ArrayList<Reservation>();
+    public static ArrayList<Review> reviewlist = new ArrayList<Review>();
+    public static ArrayList<Category> categorylist= new ArrayList<Category>();
+    
+    
+    public static void main(String args[])
+            
+            
+    {
+        
+   
+   User user1 = new User("nikos","1234","nik@gmail.com", "path1");
+   
+   userlist.add(user1);
+     
+   Administrator admin1 = new Administrator("giorgos", "12345", "giorgos@gmail.com");
+   
+   adminlist.add(admin1);
+   
+   HomePage page1 = new HomePage();
+   
+   page1.setVisible(true);
+   
+   
+   
+        
+    }
+   
+   public static User get_user(String username)
+           
+   {
+       User temp=null;
+       
+       for(User u: Main.userlist)
+           
+       {
+           
+           if(u.get_username().equals(username))
+           {
+               temp = u;
+           }
+       }
+       
+       return temp;
+   }
+   
+   
+   
+   public static Administrator get_admin(String username)
+           
+   {
+       Administrator temp=null;
+       
+       for(Administrator a: Main.adminlist)
+           
+       {
+           
+           if(a.get_username().equals(username))
+           {
+               temp = a;
+           }
+       }
+       
+       return temp;
+   }
+   
+   
+    
+}
---
+++ src/Message.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 7_USER
+ */
+public class Message {
+    
+    private int code;
+    
+    private String date;
+    
+    private String hour;
+    
+    private String username_sender;
+    
+    private String username_receiver;
+    
+    private String title;
+    
+    private String content;
+    
+   public Message(int code,String date, String hour, String username_sender, String username_receiver, String title, String content )
+           
+   {  
+       this.code = code;
+       this.date = date;
+       
+       this.hour = hour;
+       
+       this.username_sender = username_sender;
+       this.username_receiver = username_receiver;
+       
+       this.title = title;
+       this.content = content;
+   }
+   
+   
+   int get_code()
+           
+   {
+    return code;   
+   }
+   
+   String get_date()
+           
+   {
+       
+    return date;   
+   }
+   
+   String get_hour()
+           
+   {
+       
+    return hour;   
+   }
+   
+String get_sender()
+        
+{
+    return username_sender;
+}
+   
+String get_receiver()
+        
+{
+    
+ return username_receiver;   
+}
+
+String get_title()
+        
+{
+    return title;
+}
+
+String get_content()
+        
+{
+    
+ return content;   
+}
+}
+
+
+
+
+   
+    
+
