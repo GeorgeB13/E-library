54b5ddc1556bbac851eff4f169252f708f79731f
AnastasiaVelaeti <65954764+AnastasiaVelaeti@users.noreply.github.com>
2020-05-27 21:03:25 +0300

    Add files via upload

---
+++ src/Category.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 7_USER
+ */
+public class Category {
+    
+    private String name;
+    private int count;
+    
+    public Category(String name, int count)
+            
+    {
+    this.name = name;
+    this.count = count;
+    }
+    
+   String get_name()
+           
+   {
+    return name;   
+   }
+   
+   int get_count()
+           
+   {
+    return count;   
+   }
+   }
+    
+    
