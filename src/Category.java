4a3f8016bc7cbb0f1dc904f54daaa6f553bd40db
AnastasiaVelaeti <65954764+AnastasiaVelaeti@users.noreply.github.com>
2020-05-28 16:10:28 +0300

    Add files via upload

--- src/Category.java
+++ src/Category.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author 7_USER
- */
 public class Category {
     
     private String name;
