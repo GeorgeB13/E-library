ff5b7601af30baf89aee4c9d0f9750dbfd99e704
GeorgeB13 <58052536+GeorgeB13@users.noreply.github.com>
2020-05-28 16:24:12 +0300

    Update Administrator.java

--- src/Administrator.java
+++ src/Administrator.java
@@ -1,13 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author 7_USER
- */
 public class Administrator {
     
     private String username;
@@ -18,10 +8,10 @@ public class Administrator {
     public Administrator(String username, String password, String email)
             
     {
-   this.username = username;
+    this.username = username;
     this.password = password;
     this.email = email;
-}
+    }
     
     
     public String get_username()
@@ -33,22 +23,21 @@ public class Administrator {
     public String get_password()
             
     {
-     return password;   
+        return password;   
     }
     
     
-public String get_email()
+    public String get_email()
 
-{
-return email;
-}
+    {
+        return email;
+    }
 
 
-public void delete_user(User u)
-{
-    Main.userlist.remove(u);
-    
-}   
+    public void delete_user(User u)
+    {
+        Main.userlist.remove(u);
+    }   
         
         
 }
