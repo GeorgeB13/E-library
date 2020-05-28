a99342becfb273d7cafddaec175fe967627ff589
GeorgeB13 <58052536+GeorgeB13@users.noreply.github.com>
2020-05-28 17:00:27 +0300

    Update BookSearchPageForm.java

--- src/BookSearchPageForm.java
+++ src/BookSearchPageForm.java
@@ -1,18 +1,6 @@
 import java.util.*;
 import javax.swing.table.DefaultTableModel;
 
-/*
-
-
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author antonisgiannakop
- */
 public class BookSearchPageForm extends javax.swing.JFrame {
     
    ArrayList<Book> mylist = new ArrayList<Book>();
