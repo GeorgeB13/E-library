a196fb9253765356ee42cecf2c7ea17f728276cd
AnastasiaVelaeti <65954764+AnastasiaVelaeti@users.noreply.github.com>
2020-05-30 17:37:53 +0300

    Add files via upload

--- src/User.java
+++ src/User.java
@@ -40,6 +40,8 @@ public class User {
     return photopath;   
    }
    
+   
+   
    ArrayList<Book> search_book_title(String s)
            
    {
@@ -58,4 +60,137 @@ public class User {
      }
           return temp; 
    }
+   
+   
+
+ ArrayList<Book> find_book_author(String a)
+{
+    ArrayList<Book> temp = new ArrayList<Book>();
+    int flag = 0;
+     for(Book b: Main.booklist)
+   {
+       if(b.get_author().equals(a))
+       {
+           temp.add(b);
+        }
+    }
+       return temp; 
+   }
+   
+   
+   
+ArrayList<Book> search_book_category(String s)
+{
+   ArrayList<Book> temp = new ArrayList<Book>();
+   int flag = 0;
+   for(Book b: Main.booklist)
+   {
+        if(b.get_category().get_name().equals(s))
+        {
+            temp.add(b);
+         }
+    }
+        return temp; 
+   }
+   
+
+   
+   public void sumbit_review(Book b,String content)
+   { 
+       for (Book b:Main.booklist)
+       if (b.get_review.get_isbn().equals(isbn))
+       {
+           Main.reviewlist.add(content);
+       }
+   }
+   
+  
+  public void delete_account()
+  {
+       for( User u: Main.userlist)
+       {
+       if (u.get_username().equals(username))
+		{
+			Main.userlist.remove(u);
+        }
+       
+		} 
+   }
+   
+  
+public void cancel_reservation(String username)
+{
+   for( Reservation r: Main.reservationlist)
+    {   
+       if(r.get_username().equals(username))
+       {  
+           Main.reservationlist.remove(r);
+       }
+       
+   }
+   
+}
+  
+     
+   
+ArrayList<Reservation> view_reserved_books ()
+{   
+    ArrayList<Reservation> temp = new ArrayList<Reservation>();
+    int flag = 0;
+       for(Reservation r: Main.reservationlist)
+        {
+         
+            if(r.get_username().equals(username))
+            {
+                temp.add(r);
+            }
+       }
+          return temp; 
+} 
+   
+   
+ArrayList<Message> view_messages()
+{
+    ArrayList<Message> temp = new ArrayList<Message>();
+            int flaf=0;
+  
+        for(Message m: Main.usermessagelist)
+        {
+            
+            if(m.get_receiver().equals(username))
+            {
+                 temp.add(m);
+            }
+  
+        
+            return temp;
+        }
+  
+}  
+  
+
+
+ArrayList<Booking> view_user_history(String username,String isbn)
+{
+    ArrayList<Booking> temp = new ArrayList<Booking>();
+    int flag=0;
+   
+    for(Booking b:Main.bookinglist)
+    {
+    
+        if(b.get_username().equals(username))
+        {
+            temp.add(b);
+        }
+    }
+    
+    return temp;
+}
+
+
+
+
+
+
+
 }
